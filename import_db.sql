889bf58f84caa77567e838e4350f638d755d7c87
kcho760 <kcho760@gmail.com>
2023-04-05 09:42:47 -0400

    Added user id

--- import_db.sql
+++ import_db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE users
-    fname TEXT NOT NULL
-    lname TEXT NOT NULL
-    id INTEGER NOt NULL
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL,
+    id INTEGER PRIMARY KEY,
 
 CREATE TABLE questions
-    title TEXT NOT NULL
-    body TEXT NOT NULL
-    author_id INTEGER NOT NULL
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER PRIMARY KEY,
 
     FOREIGN KEY (author_id) REFERENCES users(id)
 
