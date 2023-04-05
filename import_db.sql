03884daff6490abe669f4fd8f9e5dabfc5795521
kcho760 <kcho760@gmail.com>
2023-04-05 09:43:14 -0400

    Fixed unsaved file

--- import_db.sql
+++ import_db.sql
@@ -1,13 +1,17 @@
+
+CREATE TABLE questions
+    questions_id INTEGER PRIMARY KEY
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER,
+    
+    FOREIGN KEY (author_id) REFERENCES users(id)
+
+
 CREATE TABLE users
     fname TEXT NOT NULL,
     lname TEXT NOT NULL,
     id INTEGER PRIMARY KEY,
 
-CREATE TABLE questions
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    author_id INTEGER PRIMARY KEY,
-
-    FOREIGN KEY (author_id) REFERENCES users(id)
-
 CREATE TABLE question_follows
+    
