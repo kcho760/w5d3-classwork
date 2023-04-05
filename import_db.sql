64afe78b48ac5e619410ab184bf548f107bd1310
kcho760 <kcho760@gmail.com>
2023-04-05 10:49:04 -0400

    debugged bugs

--- import_db.sql
+++ import_db.sql
@@ -1,31 +1,44 @@
+PRAGMA foreign_keys = ON;
 
-CREATE TABLE questions
-    id SERIAL PRIMARY KEY
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    author_id SERIAL,
-    
+    author_id INTEGER NOT NULL,
     FOREIGN KEY (author_id) REFERENCES users(id)
+);
 
-
-CREATE TABLE users
+CREATE TABLE users (
     fname TEXT NOT NULL,
     lname TEXT NOT NULL,
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY
+);
 
-CREATE TABLE question_follows
-    user_id INTEGER 
-    FOREIGN KEY (user_id) REFERENCES questions(id)
-    questions_id INTEGER 
-    FOREIGN KEY (questions_id) REFERENCES users(id)
-
-CREATE TABLE replies
-    id SERIAL PRIMARY KEY 
-    question_id INTEGER 
+CREATE TABLE question_follows (
+    user_id INTEGER ,
+    question_id INTEGER ,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
-    reply_id INTEGER REFERENCES replies(id)
-    user_id INTEGER 
+);
+
+CREATE TABLE replies (
+    id SERIAL PRIMARY KEY,
+    question_id INTEGER,
+    user_id INTEGER,
+    reply_body TEXT NOT NULL,
+    reply_id INTEGER,
+
+    FOREIGN KEY (reply_id) REFERENCES replies(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
+CREATE TABLE question_likes (
+    user_id INTEGER,
+    question_id INTEGER,
 
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
 
