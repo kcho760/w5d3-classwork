cbfa65a70b09640524acf93a36eb7c9fb997a044
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-04-05 11:12:34 -0400

    insert tables

--- import_db.sql
+++ import_db.sql
@@ -1,17 +1,23 @@
 PRAGMA foreign_keys = ON;
+DROP TABLE questions;
+DROP TABLE users;
+DROP TABLE question_follows;
+DROP TABLE replies;
+DROP TABLE question_likes;
+
 
 CREATE TABLE questions (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    author_id INTEGER NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES users(id)
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE users (
     fname TEXT NOT NULL,
     lname TEXT NOT NULL,
-    id SERIAL PRIMARY KEY
+    id INTEGER PRIMARY KEY
 );
 
 CREATE TABLE question_follows (
@@ -23,7 +29,7 @@ CREATE TABLE question_follows (
 );
 
 CREATE TABLE replies (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     question_id INTEGER,
     user_id INTEGER,
     reply_body TEXT NOT NULL,
@@ -41,4 +47,55 @@ CREATE TABLE question_likes (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+INSERT INTO 
+    users( fname, lname ) 
+VALUES
+    ("Kevin", "Cho");
+
+INSERT INTO 
+    users( fname, lname ) 
+VALUES
+    ("Angelo", "Ciffone");
+
+INSERT INTO 
+    users( fname, lname ) 
+VALUES
+    ("Ayce", "Jones");
+
+INSERT INTO 
+    users( fname, lname ) 
+VALUES
+    ("Not", "Spencer");
+
+INSERT INTO 
+    users( fname, lname ) 
+VALUES
+    ("Not", "Kyle");
+
+
+INSERT INTO 
+    questions (title, body, user_id)
+VALUES 
+    ( "Why am I at App Academy", "Cause I''m broke", 1);
+
+INSERT INTO 
+    questions (title, body, user_id)
+VALUES 
+    ( "Why am I at App Academy 2", "Cause I''m broke", 3);
+
+    INSERT INTO 
+    questions (title, body, user_id)
+VALUES 
+    ( "Why am I at App Academy 3", "Cause I''m broke", 5);
+
+INSERT INTO 
+    questions (title, body, user_id)
+VALUES 
+    ( "Why am I at App Academy4", "Cause I''m broke", 1);
+
+INSERT INTO 
+    questions (title, body, user_id)
+VALUES 
+    ( "Why am I at App Academy5", "Cause I''m broke", 4);
 
